3a9397635534f4a84c112b1d7d4f68a15030e262
Surajk37 <surajkhot9993@gmail.com>
2023-01-12 21:50:35 +0530

    Welcome Message added in master branch

---
+++ AlgorithmPrograms/Program.cs
@@ -0,0 +1,11 @@
+﻿namespace AlgorithmPrograms
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("\t\tWelcome To Algorithm Programs\n");
+            Console.ReadLine();
+        }
+    }
+}
